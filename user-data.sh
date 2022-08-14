115c0edf0b55ad2f6e11408a462dba4c3cde86d4
sompatil <105210730+sompatil@users.noreply.github.com>
2022-08-14 16:13:14 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,5 +12,5 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 cd /var/www/html
-curl http://169.254.169.254/latest/meta-data/instance-id -o index.html
+curl http://54.234.208.144/latest/meta-data/instance-id -o index.html
 curl https://raw.githubusercontent.com/hashicorp/learn-terramino/master/index.php -O
